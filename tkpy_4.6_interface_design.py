f66e7293db4153b93b9f7dccc060fa15dca4c779
Zhu Yun <yzhu.andrew@163.com>
2014-12-15 22:16:53 +0800

    add some new files

---
+++ tkpy_4.6_interface_design.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import math
+
+__author__ = 'Yun'
+__project__ = 'PythonCookbook_Test'
+
+
+def circle(t, r):
+    circumference = 2 * math.pi * r
+    n = 50
+    length = circumference / n
