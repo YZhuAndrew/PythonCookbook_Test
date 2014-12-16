a0bcf7f5a8f6d6ec320fe47eaf1cffd3453bc43a
Zhu Yun <yzhu.andrew@163.com>
2014-12-16 21:55:14 +0800

    do some work in tkpy_4.6_interface_design.py

--- tkpy_4.6_interface_design.py
+++ tkpy_4.6_interface_design.py
@@ -5,7 +5,90 @@ __author__ = 'Yun'
 __project__ = 'PythonCookbook_Test'
 
 
-def circle(t, r):
-    circumference = 2 * math.pi * r
-    n = 50
-    length = circumference / n
+# def circle(t, r):
+# circumference = 2 * math.pi * r
+# n = 50
+# length = circumference / n
+
+
+# def sequence(n):
+#     while n != 1:
+#         print n
+#         if n % 2 == 0:      # n is even
+#             n /= 2
+#         else:               # n is odd
+#             n = n * 3 + 1
+#
+# sequence(3)
+# for i in range(100):
+#     if i > 0:
+#         sequence(i)
+
+
+# x = 3
+# a = 5
+#
+# while True:
+#     print x
+#     y = (x + a/x) / 2
+#     if y == x:
+#         break
+#     x = y
+
+
+# def find(word, letter):
+#     for eachletter in word:
+#         if eachletter == letter:
+#             return word.index(eachletter)
+#     return -1
+
+# print find('yzhuandrew', 'd')
+
+
+# def count_letter(word, letter):
+#     count_num = 0
+#     for eachletter in word:
+#         if eachletter == letter:
+#             count_num += 1
+#     return count_num
+#
+# print count_letter('aaaaaaaaaaaaaaaaabbbbccc', 'b')
+# print count_letter('aaaaaaaaaaaaaaaaabbbbccc', 'bb')
+# print count_letter('aaaaaaaaaaaaaaaaabbbbccc', 'cc')
+
+# print 'aaabbccc'.find('cc')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
