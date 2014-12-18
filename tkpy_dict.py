f95735858f831086f3e931a3dfb8428400339d91
Zhu Yun <yzhu.andrew@163.com>
2014-12-18 22:55:23 +0800

    save the project from my work pc - 2014-12-19

---
+++ tkpy_dict.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Yun'
+__project__ = 'PythonCookbook_Test'
+
+'''
+def count_letter(letters):
+    list_letters = list(letters)
+    list_count = [0]*len(letters)
+'''
+
