1dd27bb4225a14224991fa9225981b2ee4cfd339
Zhu Yun <yzhu.andrew@163.com>
2014-12-18 22:55:23 +0800

    save the project in my home pc - 2014-12-18

---
+++ tkpy_dict.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Yun'
+__project__ = 'PythonCookbook_Test'
+
+
+def count_letter(letters):
+    list_letters = list(letters)
+    list_count = [0]*len(letters)
+    dict_count = {} for list_letters, list_count
